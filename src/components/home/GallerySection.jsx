05ca0fef55fad4f0ca315b2063c0cbfc3cdb57d6
mosh-d <shopsy8@gmail.com>
2026-05-03 15:20:24 +0100

    Correcting images

--- src/components/home/GallerySection.jsx
+++ src/components/home/GallerySection.jsx
@@ -109,7 +109,9 @@ export default function GallerySection() {
   ];
 
   // Select appropriate gallery based on screen size
-  const galleryImages = isMobile ? mobileGalleryImages : desktopGalleryImages;
+  // const galleryImages = isMobile ? mobileGalleryImages : desktopGalleryImages;
+
+  const galleryImages = desktopGalleryImages;
 
   const openModal = (index) => {
     setCurrentImageIndex(index);
