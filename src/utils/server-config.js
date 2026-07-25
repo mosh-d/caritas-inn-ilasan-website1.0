0382fc9112d8ea6c0a32d0eee60890b8aed60938
mosh-d <shopsy8@gmail.com>
2026-07-25 19:00:48 +0100

    Switching to NestJs backend

--- src/utils/server-config.js
+++ src/utils/server-config.js
@@ -1,5 +1,5 @@
-export const SERVER_BASE_URL = "http://localhost:3000";
-// export const SERVER_BASE_URL = "https://five-clover-nestjs-backend.onrender.com";
+// export const SERVER_BASE_URL = "http://localhost:3000";
+export const SERVER_BASE_URL = "https://five-clover-nestjs-backend.onrender.com";
 // export const SERVER_BASE_URL = "https://five-clover-shared-backend.onrender.com";
 // Use the production URL for deployment. For local testing, comment out the production line above and uncomment the localhost line.
 
